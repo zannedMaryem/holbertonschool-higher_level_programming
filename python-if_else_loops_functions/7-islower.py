50242cc5ddb3f7bc6316348f5c0b954fe50fb7f3
maryem <zanned.maryem@gmail.com>
2026-05-06 16:38:41 +0100

    add 7-islower.py file

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(ord('a'), ord('z')):
-        if int(c) == i:
+        if ord(c) == i:
             return(True)
         else:
             return(False)
