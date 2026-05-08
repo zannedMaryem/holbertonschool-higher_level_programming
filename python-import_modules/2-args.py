582b67bab46c49aa856e6335786c5fee9382b693
maryem <zanned.maryem@gmail.com>
2026-05-08 15:17:35 +0100

    add 2-args.py file

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         print("1 argument:\n1:", sys.argv[1])
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(2, len(sys.argv)):
+        for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
