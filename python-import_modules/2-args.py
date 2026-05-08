3e4202fed33a153c6e82934d9eb303a0818d6020
maryem <zanned.maryem@gmail.com>
2026-05-08 15:11:52 +0100

    add 2-args.py file

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
+    if len(sys.argv) == 1:
         print("0 arguments.")
     elif len(sys.argv) == 2:
         print("1 argument:\n1: ", sys.argv[1])
