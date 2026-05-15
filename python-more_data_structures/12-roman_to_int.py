3b5222da3523833e03e03e94de99d535364eb84d
maryem <zanned.maryem@gmail.com>
2026-05-15 13:55:08 +0100

    add 12-roman_to_int.py file

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    total = 0
+    prev_val = 0
+    roman_num = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+    for letter in reversed(roman_string):
+        val = roman_num[letter]
+        if val < prev_val:
+            total -= val
+        else:
+            total += val
+        prev_val =  val
+    return total
