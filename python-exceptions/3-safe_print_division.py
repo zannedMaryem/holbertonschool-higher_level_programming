45435d456419087ad6d52f893f60e9f0410ac4eb
maryem <zanned.maryem@gmail.com>
2026-05-16 13:16:29 +0100

    add 3-safe_print_division.py file

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except(ZeroDivisionError, ValueError):
+        pass
+    finally:
+        print("Inside Result: {}".format(result))
+    return result
