2c6c948b1121ff63a6d3557a32de5308420ada4b
maryem <zanned.maryem@gmail.com>
2026-05-17 17:52:53 +0100

    fixed pycodestyel 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 """
-This module provides a function that adds two integers, 
-ensuring type safety by raising a TypeError 
+This module provides a function that adds two integers,
+ensuring type safety by raising a TypeError
 when inputs are not integers or floats.
 """
+
+
 def add_integer(a, b=98):
     """
-    Adds two integers or floats after casting 
+    Adds two integers or floats after casting
     them to integers.
 
     Args:
         a: First number, must be int or float.
-        b: Second number, must be int or float 
+        b: Second number, must be int or float
         (default is 98).
 
     Returns:
         The integer sum of a and b.
 
     Raises:
-        TypeError: If a or b are not 
+        TypeError: If a or b are not
         integers or floats.
 
     """
