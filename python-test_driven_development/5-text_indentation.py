20c3a846aadfd504af524658b5e6a3902ae51b88
maryem <zanned.maryem@gmail.com>
2026-05-17 19:02:14 +0100

    add  5-text_indentation.py tests/5-text_indentation.txt files

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -19,13 +19,14 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     buffer = ""
+
     for char in text:
         buffer += char
-        if char in ".?:":
-            # print the current buffer (stripped of leading/trailing spaces)
-            print(buffer.strip(), end="\n\n")
-            buffer = ""  # reset buffer
 
-    # print any leftover text (if it doesn't end with . ? :)
+        if char in ".?:":
+            print(buffer.strip())
+            print()
+            buffer = ""
+
     if buffer.strip():
         print(buffer.strip(), end="")
