ab9a2925f766f53bd08d62151d54b064a5793dbc
maryem <zanned.maryem@gmail.com>
2026-05-17 18:02:51 +0100

    add 3-say_my_name.py tests/3-say_my_name.txt file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines a function that prints a formatted name.
+"""
+
+
 def say_my_name(first_name, last_name=""):
     """
     Prints: My name is <first name> <last name>
