6d75a00249b50215902ba41f4d3573f89aa8ac1c
maryem <zanned.maryem@gmail.com>
2026-05-20 14:41:30 +0100

    add 2-square.py file

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,5 +10,5 @@ class Square():
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size <= 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
