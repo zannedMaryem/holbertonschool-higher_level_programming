2f5fe725c61a1d8fa0ebaf2c4c093de99b11ded0
maryem <zanned.maryem@gmail.com>
2026-05-20 14:37:53 +0100

    add 2-square.py file

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square():
+    """A class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a private size attribute."""
+        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise ValueError("size must be >= 0")
