bc0bfc5854874f4dbef443e0525a9aa4bc909365
maryem <zanned.maryem@gmail.com>
2026-05-22 15:25:40 +0100

    add 3-rectangle.py file

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -55,6 +55,7 @@ class Rectangle():
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2*(self.__width + self.__height)
+
     def __str__(self):
         """Return the rectangle as a string of '#' characters."""
         if self.__width == 0 or self.__height == 0:
