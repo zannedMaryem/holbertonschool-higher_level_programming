e141a1867f5971e1bc2612a18dc90e31f262833a
maryem <zanned.maryem@gmail.com>
2026-05-22 16:44:54 +0100

    add 9-rectangle.py file

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -88,9 +88,9 @@ class Rectangle():
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
+
     @classmethod
     def square(cls, size=0):
         """ returns a new Rectangle instance with
             width == height == size"""
         return cls(size, size)
-    
