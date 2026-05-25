6f04511b7455ce7631b7c435084db83c1d678130
maryem <zanned.maryem@gmail.com>
2026-05-25 13:41:29 +0100

    add tests/1-my_list.txt 1-my_list.py file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Class that sorts a list"""
+
+
+class MyList(list):
+    """ Inherited class from list"""
+
+    def print_sorted(self):
+        """ a class methood to sort list"""
+        return(sorted(self))
