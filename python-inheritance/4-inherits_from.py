4dfb58bc70e08a4365aa52f150dfcc4d5c51ccf7
maryem <zanned.maryem@gmail.com>
2026-05-25 16:08:20 +0100

    add 4-inherits_from.py file

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module checks if an object is is an
+instance of a class that inherited
+(directly or indirectly) from the specified class"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of
+    a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False"""
+    return issubclass(obj, a_class)
