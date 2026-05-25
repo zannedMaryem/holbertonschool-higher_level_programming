d98b56ce8b082de3140c9e3cfd6570b87757bc88
maryem <zanned.maryem@gmail.com>
2026-05-25 15:37:08 +0100

    add updates to 1-my_list.py file

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,10 +5,6 @@
 class MyList(list):
     """ Inherited class from list"""
 
-    def __str__(self):
-        """Return the string representation of the list."""
-        return str(list(self))
-
     def print_sorted(self):
         """Print the list sorted in ascending order."""
         print(sorted(self))
