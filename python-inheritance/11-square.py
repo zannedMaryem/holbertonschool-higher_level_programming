f2f9d7fe6e4bf586ec666aa929c752b7530363e1
maryem <zanned.maryem@gmail.com>
2026-05-25 19:11:07 +0100

    add 11-square.py   file

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module is for defining basic a square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class that defines a square"""
+
+    def __init__(self, size):
+        """Init method for square"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Method to calculate square area"""
+        return self.__size ** 2
