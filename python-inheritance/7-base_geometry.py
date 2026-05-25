aa37823172f92f9a2feb5c703539894138209322
maryem <zanned.maryem@gmail.com>
2026-05-25 18:15:47 +0100

    add 7-base_geometry.py tests/7-base_geometry.txt files

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry():
     def integer_validator(self, name, value):
         """A method that validates a value"""
         self.name = name
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{self.name} must be an integer")
         if value <= 0:
             raise ValueError(f"{self.name} must be greater than 0")
