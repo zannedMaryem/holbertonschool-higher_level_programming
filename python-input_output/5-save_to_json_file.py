6c75b8a0aadeb652a8d2d29e0d086a655c765679
maryem <zanned.maryem@gmail.com>
2026-06-05 16:21:50 +0100

    add 5-save_to_json_file.py file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""This module is to write data into a file with JSON"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """This function writes data o a file"""
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(my_obj, file, indent=4, ensure_ascii=False)
