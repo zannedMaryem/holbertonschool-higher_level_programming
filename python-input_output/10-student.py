5e6ca19c280a9296d54d93b990cdf48575fc4682
maryem <zanned.maryem@gmail.com>
2026-06-05 23:31:59 +0100

    add 10-student.py file

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -16,4 +16,5 @@ class Student():
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+            return {key: value for key, value in
+                    self.__dict__.items() if key in attrs}
