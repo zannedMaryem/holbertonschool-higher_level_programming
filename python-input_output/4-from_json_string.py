34a52ccb75e03c3e318cbc5b7b7f8d6e99a779db
maryem <zanned.maryem@gmail.com>
2026-06-05 14:43:07 +0100

    add 4-from_json_string.py file

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module is to deserialize"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """This function returns an object represented by a JSON string"""
+    return json.load(my_str)
