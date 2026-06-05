e8805409e97729b79a2a9425ff9a1d521adfcc43
maryem <zanned.maryem@gmail.com>
2026-06-05 14:12:01 +0100

    add 0-read_file.py file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """This function reads a text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as myfile:
-        print(myfile.read())
+        print(myfile.read(), end="")
