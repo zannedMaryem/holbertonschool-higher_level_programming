16ffe26f51cc1c77fb698ac9ed81781fbc69b8b1
maryem <zanned.maryem@gmail.com>
2026-06-05 14:04:42 +0100

    add 0-read_file.py file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module is intended to open a file"""
+
+
+def read_file(filename=""):
+    """This function reads a text file and prints it to stdout"""
+    with open("filename", encoding="utf-8") as myfile
+    print(myfile.read())
