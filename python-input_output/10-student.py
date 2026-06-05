51d9ffb6ac01fde2e1a9b0aeddd5e28a5e98d9dd
maryem <zanned.maryem@gmail.com>
2026-06-05 23:31:04 +0100

    add 10-student.py file

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module is to create a student class"""
+
+
+class Student():
+    """This class defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
