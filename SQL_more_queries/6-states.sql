caec4df5f9a57a972f92cb2d6f552a86ff10a077
maryem <zanned.maryem@gmail.com>
2026-06-26 16:22:41 +0100

    add 6-states.sql file

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Create database and table
-CREATE DATABASE TF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
