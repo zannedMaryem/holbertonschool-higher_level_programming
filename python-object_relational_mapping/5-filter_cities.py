7d4c9588626f12b94aedab5cadd1be8ae3d17c11
maryem <zanned.maryem@gmail.com>
2026-06-27 18:25:59 +0100

    add 5-filter_cities.py file

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute("""
-        SELECT cities.id, cities.name, states.name
+        SELECT cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
         ORDER BY cities.id ASC
     """, (sys.argv[4],))
-    for row in cur.fetchall():
-        print(row)
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
     cur.close()
     db.close()
