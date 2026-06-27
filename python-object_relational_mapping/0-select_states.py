1e264f67769e67c52c30b460957f53fc192cc575
maryem <zanned.maryem@gmail.com>
2026-06-27 16:20:32 +0100

    add 0-select_states.py file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,23 +7,18 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from arguments
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    # Connect to MySQL server on localhost:3306
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password, db=db_name)
 
     cur = db.cursor()
-    # Query states sorted by id ascending
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
 
-    # Print results exactly as tuples
     for row in cur.fetchall():
         print(row)
 
-    # Close cursor and connection
     cur.close()
     db.close()
