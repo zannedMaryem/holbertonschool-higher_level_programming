95d28b0c3ad1a5c0c467fc1a19fee5406c895cb3
maryem <zanned.maryem@gmail.com>
2026-07-12 16:54:29 +0100

    add 10-factorial.js file

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n) || n <= 0) {
+    return 1;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+
+console.log(factorial(num));
