9c64f81a427b2dfaeb0cec7275d84a0108b62bad
maryem <zanned.maryem@gmail.com>
2026-07-24 15:20:59 +0100

    add task_02_logic.py file

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -17,9 +17,10 @@ def contact():
 
 @app.route('/items')
 def items():
-       with open('items.json') as f:
-              data = json.load(f)
-       return render_template('items.html', items=data['items'])
+    with open('items.json') as f:
+            data = json.load(f)
+    items_list = data.get('items', []) if isinstance(data, dict) else data
+    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
        app.run(debug=True, port=5000)
